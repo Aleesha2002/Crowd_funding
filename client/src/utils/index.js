225b8e9e9583ad0cb18a5cf1d8257bb75536556d
Aleesha2002 <aleesha.lalit@gmail.com>
2023-06-29 14:52:15 +0530

    done

---
+++ client/src/utils/index.js
@@ -0,0 +1,22 @@
+export const daysleft = (deadline) => {
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainingDays = difference / (1000 * 60 * 60 * 24);
+
+  return remainingDays.toFixed(0);
+};
+
+export const calculateBarPercentage = (goal, raisedAmount) => {
+  const percentage = Math.round((raisedAmount * 100) / goal);
+
+  return percentage;
+};
+
+export const chackIfImage = (url, callback) => {
+  const img = new Image();
+  img.src = url;
+
+  if (img.complete) callback(true);
+
+  img.onload = () => callback(true);
+  img.onerror = () => callback(false);
+};
