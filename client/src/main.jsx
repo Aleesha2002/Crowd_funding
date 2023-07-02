1fae412bc29a4c4be3948137a7d9ea31f852aa8a
Aleesha Lalit <107275325+Aleesha2002@users.noreply.github.com>
2023-07-02 19:18:50 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Sepolia } from "@thirdweb-dev/chains";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import App from "./App";
 import "./index.css";
+import { StateContextProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.sepolia}>
+  <ThirdwebProvider activeChain={Sepolia}>
     <Router>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </Router>
   </ThirdwebProvider>
 );
